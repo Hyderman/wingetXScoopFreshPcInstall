27f1a7f74abfac447995520e0e575559e84c3539
Hyderman <logan.faurisson@gmail.com>
2022-10-10 15:21:51 +0200

    update

--- dirNametojson.py
+++ dirNametojson.py
@@ -1,5 +1,4 @@
 import os
-import ntpath
 import json
 
 def dirtojson(path):
--- invokeps.py
+++ invokeps.py
@@ -2,10 +2,11 @@ import subprocess
 import json
 
 def psCommand(cmd):
-    subprocess.run(["powershell", "command", cmd]) 
+    subprocess.run(["powershell", "-Command", cmd], capture_output=True) 
 
 if __name__ == "__main__":
     with open("scoopPackage.json") as f:
         dirlist = json.load(f)
         for dir in dirlist:
-            psCommand(f"scoop install {dir}")
+            cmd = f"scoop install {dir}"
+            psCommand(cmd)
