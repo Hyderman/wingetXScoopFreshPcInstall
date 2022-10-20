9188e10a08075a36ccff414c59a37f0cceb2d4ee
Hyderman <logan.faurisson@gmail.com>
2022-10-21 01:46:28 +0200

    check for scoop app

--- invokeps.py
+++ invokeps.py
@@ -2,11 +2,12 @@ import subprocess
 import json
 
 def psCommand(cmd):
-    subprocess.run(["powershell", "-Command", cmd], capture_output=True) 
+    subprocess.run(["powershell", "-Command", cmd]) 
 
 if __name__ == "__main__":
     with open("scoopPackage.json") as f:
         dirlist = json.load(f)
         for dir in dirlist:
-            cmd = f"scoop install {dir}"
-            psCommand(cmd)
+            if dir != "scoop":
+                cmd = f"scoop install {dir}"
+                psCommand(cmd)
