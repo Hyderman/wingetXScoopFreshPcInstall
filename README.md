785fa091c0d5c3582035fb4264aab2c17f2e4ae9
Hyderman <logan.faurisson@gmail.com>
2022-10-21 02:06:25 +0200

    add bucket install

---
+++ README.md
@@ -0,0 +1,5 @@
+# Steps
+
+1. Create json file with "dirNametojson.py"
+2. Add necessary buckets to "psInvokeScoop.py"
+3. Install package with "psInvokeScoop.py"
--- psInvokeScoop.py
+++ psInvokeScoop.py
@@ -1,8 +1,14 @@
 import subprocess
 import json
 
-def psCommand(cmd):
+def psCommandPackage(cmd):
     subprocess.run(["powershell", "-Command", cmd]) 
+
+
+def psCommandBucket():
+    cmd = "scoop bucket add main; scoop bucket add extras"
+    subprocess.run(["powershell", "-Command", cmd]) 
+
 
 if __name__ == "__main__":
     with open("scoopPackage.json") as f:
@@ -10,4 +16,4 @@ if __name__ == "__main__":
         for dir in dirlist:
             if dir != "scoop":
                 cmd = f"scoop install {dir}"
-                psCommand(cmd)
+                psCommandPackage(cmd)
