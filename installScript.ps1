2c3fceea63fe194b3777c39e902f0b7ea1334119
Hyderman <logan.faurisson@gmail.com>
2023-04-05 00:03:53 +0200

    solve some error from scoop

--- installScript.ps1
+++ installScript.ps1
@@ -1,11 +1,13 @@
-winget install powershell --force
+winget install Microsoft.PowerShell --force
 pwsh -Command {
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-    irm get.scoop.sh | iex
+    if (!(Get-Command scoop)) {
+        irm get.scoop.sh | iex
+    }
     scoop install aria2
     scoop install python
     python childScript.py
-    Copy-Item -Path $env:USERPROFILE\Programmation\ScoopInstallMultiplePackage\Microsoft.PowerShell_profile.ps1 -Destination $env:USERPROFILE/Documents/PowerShell/Microsoft.PowerShell_profile.ps1 
-    Install-Module Pscx -Scope CurrentUser
+    Copy-Item -Path .\Microsoft.PowerShell_profile.ps1 -Destination $env:USERPROFILE/Documents/PowerShell/Microsoft.PowerShell_profile.ps1 
+    Install-Module -Name Pscx -AllowClobber
     Install-Module -Name Terminal-Icons -Repository PSGallery
 }
