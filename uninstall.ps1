0d91420f68850978716d0be418d3261f60b7b574
Hyderman <logan.faurisson@gmail.com>
2023-04-08 00:28:00 +0200

    Remove temp file

--- uninstall.ps1
+++
@@ -1,9 +0,0 @@
-sudo Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
-    -Name "ConsentPromptBehaviorAdmin" `
-    -Value 0
-    
-$wingetUninstall = Get-Content -Raw -Path "./wingetUninstallBloatware.json" | ConvertFrom-Json
-
-$wingetUninstall | ForEach-Object {
-    sudo winget uninstall $_
-}
